b90d6cc074ebf1d6f5bc7e51519e9f4124a0f0da
mamorucom <eetsutto@gmail.com>
2022-03-29 12:14:53 +0900

    showCupertinoDialogとの切り分け用を実装

--- lib/pages/dialog_sample_page.dart
+++ lib/pages/dialog_sample_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dialog_sample/pages/widgets/text_field_dialog_button_widget2.dart';
 import 'widgets/alert_dialog_button_widget.dart';
 import 'widgets/drop_down_dialog_button_widget.dart';
 import 'widgets/multiple_dialog_button_widget.dart';
@@ -37,6 +38,11 @@ class DialogSamplePage extends StatelessWidget {
           DropdownDialogButtonWidget(),
 
           TextFieldDialogButtonWidget(),
+
+          TextFieldDialogButtonWidget2(),
+
+          
+
 
           // MultipleDialogButtonWidget(),
 
