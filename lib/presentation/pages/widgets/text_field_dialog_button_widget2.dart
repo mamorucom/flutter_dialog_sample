f87e0d798a18fa7faee33a587595b50e126d831c
mamorucom <eetsutto@gmail.com>
2022-03-29 18:56:02 +0900

    builderにまとめた

--- lib/presentation/pages/widgets/text_field_dialog_button_widget2.dart
+++ lib/presentation/pages/widgets/text_field_dialog_button_widget2.dart
@@ -15,140 +15,83 @@ class TextFieldDialogButtonWidget2 extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final nameController = useTextEditingController();
     final numberController = useTextEditingController();
+
+    final builder = CustomTextFieldDialog(
+      title: 'テキストフィールドダイアログ2',
+      contentWidget: Card(
+        color: Colors.transparent,
+        elevation: 0.0,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextFormField(
+              controller: nameController,
+              maxLength: 10,
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              keyboardType: TextInputType.text,
+              textInputAction: TextInputAction.next,
+              decoration: const InputDecoration(
+                labelText: '名前',
+                errorMaxLines: 2,
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  // return 'Name must not be null or empty.';
+                  return '名前を入力してください。';
+                }
+                if (value.length > 10) {
+                  return '';
+                }
+                return null;
+              },
+            ),
+            TextFormField(
+              controller: numberController,
+              maxLength: 10,
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              keyboardType: TextInputType.number,
+              textInputAction: TextInputAction.next,
+              decoration: const InputDecoration(
+                labelText: '番号',
+                errorMaxLines: 2,
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  // return 'Number must not be null or empty.';
+                  return '番号を入力してください。';
+                }
+                if (value.length > 10) {
+                  return '';
+                }
+                return null;
+              },
+            ),
+          ],
+        ),
+      ),
+      cancelActionText: 'Cancel',
+      cancelAction: () {},
+      defaultActionText: 'OK',
+      action: () {
+        // TODO: implement method
+      },
+    );
+
     return ElevatedButton(
-        child: const Text('テキストフィールドダイアログボタン2'),
-        onPressed: () {
-          if (kIsWeb || Platform.isAndroid) {
-            showDialog(
-              context: context,
-              builder: (context) => CustomTextFieldDialog(
-                title: 'テキストフィールドダイアログ2',
-                contentWidget: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    TextFormField(
-                      controller: nameController,
-                      maxLength: 10,
-                      autovalidateMode: AutovalidateMode.onUserInteraction,
-                      keyboardType: TextInputType.text,
-                      textInputAction: TextInputAction.next,
-                      decoration: const InputDecoration(
-                        labelText: '名前',
-                        errorMaxLines: 2,
-                      ),
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          // return 'Name must not be null or empty.';
-                          return '名前を入力してください。';
-                        }
-                        if (int.tryParse(value) == null) {
-                          return '番号を入力してください。';
-                        }
-                        if (value.length > 10) {
-                          return '';
-                        }
-                        return null;
-                      },
-                    ),
-                    TextFormField(
-                      controller: numberController,
-                      maxLength: 10,
-                      autovalidateMode: AutovalidateMode.onUserInteraction,
-                      keyboardType: TextInputType.number,
-                      textInputAction: TextInputAction.next,
-                      decoration: const InputDecoration(
-                        labelText: '番号',
-                        errorMaxLines: 2,
-                      ),
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          // return 'Number must not be null or empty.';
-                          return '番号を入力してください。';
-                        }
-                        if (int.tryParse(value) == null) {
-                          return '番号を入力してください。';
-                        }
-                        if (value.length > 10) {
-                          return '';
-                        }
-                        return null;
-                      },
-                    ),
-                  ],
-                ),
-                cancelActionText: 'Cancel',
-                cancelAction: () {},
-                defaultActionText: 'OK',
-                action: () {
-                  // TODO: implement method
-                },
-              ),
-            );
-          } else {
-            showCupertinoDialog(
-              context: context,
-              builder: (context) => CustomTextFieldDialog(
-                title: 'テキストフィールドダイアログ2',
-                contentWidget: Card(
-                  color: Colors.transparent,
-                  elevation: 0.0,
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      TextFormField(
-                        controller: nameController,
-                        maxLength: 10,
-                        autovalidateMode: AutovalidateMode.onUserInteraction,
-                        keyboardType: TextInputType.text,
-                        textInputAction: TextInputAction.next,
-                        decoration: const InputDecoration(
-                          labelText: '名前',
-                          errorMaxLines: 2,
-                        ),
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            // return 'Name must not be null or empty.';
-                            return '名前を入力してください。';
-                          }
-                          if (value.length > 10) {
-                            return '';
-                          }
-                          return null;
-                        },
-                      ),
-                      TextFormField(
-                        controller: numberController,
-                        maxLength: 10,
-                        autovalidateMode: AutovalidateMode.onUserInteraction,
-                        keyboardType: TextInputType.number,
-                        textInputAction: TextInputAction.next,
-                        decoration: const InputDecoration(
-                          labelText: '番号',
-                          errorMaxLines: 2,
-                        ),
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            // return 'Number must not be null or empty.';
-                            return '番号を入力してください。';
-                          }
-                          if (value.length > 10) {
-                            return '';
-                          }
-                          return null;
-                        },
-                      ),
-                    ],
-                  ),
-                ),
-                cancelActionText: 'Cancel',
-                cancelAction: () {},
-                defaultActionText: 'OK',
-                action: () {
-                  // TODO: implement method
-                },
-              ),
-            );
-          }
-        });
+      child: const Text('テキストフィールドダイアログボタン2'),
+      onPressed: () {
+        if (kIsWeb || Platform.isAndroid) {
+          showDialog(
+            context: context,
+            builder: (context) => builder,
+          );
+        } else {
+          showCupertinoDialog(
+            context: context,
+            builder: (context) => builder,
+          );
+        }
+      },
+    );
   }
 }
